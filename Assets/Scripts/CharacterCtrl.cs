a17ee88a5bd67933acae825af26ad1a970d8b98e
Jackson Nelson-Gal <jjn58@case.edu>
2020-02-01 00:22:08 -0500

    Added basic player movement including staircases.

---
+++ Assets/Scripts/CharacterCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCtrl : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float walkSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = (Input.GetAxis("Horizontal") != 0) ? Input.GetAxis("Horizontal") * walkSpeed : 0;
+        
+        rb.velocity = Vector2.up * rb.velocity + Vector2.right * movement;
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            StairCtrl stair = collider.GetComponent<StairCtrl>();
+            if (stair != null)
+            {
+                stair.Travel(transform, Input.GetAxis("Vertical") > 0);
+            }
+        }
+    }
+}
