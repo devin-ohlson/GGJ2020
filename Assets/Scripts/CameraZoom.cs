0effb77d411d126219daf555186fab7e75a37a35
Christian <cbonnell28@gmail.com>
2020-02-02 11:19:39 -0500

    Camera Zoom now functioning. Need to stop character from moving while zoomed out

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField]
+    float zoomFactor = 1.0f;
+
+    [SerializeField]
+    float zoomSpeed = 5.0f;
+
+    private float originalSize = 0f;
+
+    private Camera thisCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        thisCamera = GetComponent<Camera>();
+        originalSize = thisCamera.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float targetSize = originalSize * zoomFactor;
+        if (targetSize != thisCamera.orthographicSize)
+        {
+            thisCamera.orthographicSize = Mathf.Lerp(thisCamera.orthographicSize, targetSize, Time.deltaTime * zoomSpeed);
+        }
+    }
+
+    public void SetZoom(float zoomFactor)
+    {
+        this.zoomFactor = zoomFactor;
+    }
+}
--- Assets/Scripts/CharacterCtrl.cs
+++ Assets/Scripts/CharacterCtrl.cs
@@ -5,13 +5,67 @@ using UnityEngine;
 public class CharacterCtrl : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Vector3 roomPosition;
+    private bool cameraAnchored = false;
+    public GameObject anchor;
+    public CameraZoom cameraZoom;
+	[SerializeField] private float minRangeToTransform;
+	[SerializeField] private float lerpSpeed;
+	private float initialZ;
+    private CameraController mainCam;
+    [SerializeField]
+    float zoomFactor = 1.0f;
+
+    [SerializeField]
+    float zoomSpeed = 5.0f;
+    private float originalSize = 0f;
+
+
     [SerializeField] private float walkSpeed = 5;
     
     void Start()
     {
+        Debug.Log(Camera.main.fieldOfView);
         rb = GetComponent<Rigidbody2D>();
+        mainCam = Camera.main.gameObject.GetComponent<CameraController>();
+        cameraZoom = Camera.main.gameObject.GetComponent<CameraZoom>();
     }
     
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Q)){
+            if(cameraAnchored){
+                Debug.Log("Moving to camera anchor");
+                mainCam.LerpToPosition(roomPosition);
+                cameraZoom.SetZoom(1);
+                cameraAnchored = false;
+            }else{
+                Debug.Log("Moving to camera to character");
+                roomPosition = mainCam.gameObject.transform.position;
+                mainCam.LerpToPosition(anchor.transform.position);
+                cameraZoom.SetZoom(3);
+                cameraAnchored = true;
+            }
+        }
+    }
+
+    public void LerpToPosition(Vector3 target, int fov) {
+		StopAllCoroutines();
+		StartCoroutine(LerpToPositionCoroutine(target, fov));
+	}
+	
+	private IEnumerator LerpToPositionCoroutine(Vector3 targetPosition, int fov) {
+		while(Vector3.Distance(transform.position, targetPosition) > minRangeToTransform) {
+			transform.position = Vector3.Lerp(transform.position, targetPosition, lerpSpeed);
+
+            Camera.main.fieldOfView = fov;
+
+			Vector3 fixedZ = transform.position;
+			fixedZ.z = initialZ;
+			transform.position = fixedZ;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
     void FixedUpdate()
     {
         float movement = (Input.GetAxis("Horizontal") != 0) ? Input.GetAxis("Horizontal") * walkSpeed : 0;
