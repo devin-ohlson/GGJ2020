863641d3eed037fb77bf0989df8368cd8752a813
Christian <cbonnell28@gmail.com>
2020-02-02 13:04:52 -0500

    Zoomout and minimap popup now functioning. Need to link to rooms

---
+++ Assets/Scripts/AlertController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlertController : MonoBehaviour
+{
+
+    [SerializeField]public Image alertIcon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alertIcon = GetComponent<Image>();
+    }
+
+    void Update(){
+		if (Input.GetKeyDown(KeyCode.R)) {
+            DeActivateAlert();
+		}
+        else if (Input.GetKeyDown(KeyCode.B)){
+            ActivateAlert();
+        }
+	}
+
+    public void ActivateAlert(){
+        alertIcon.color = new Color(alertIcon.color.r, alertIcon.color.g, alertIcon.color.b, 1f);
+    }
+
+    public void DeActivateAlert(){
+        alertIcon.color = new Color(alertIcon.color.r, alertIcon.color.g, alertIcon.color.b, 0.1f);
+    }
+}
